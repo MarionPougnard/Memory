950f6399c217ca9e6b00518b152daac14ef96bea
Marion Pougnard <marion.pougnard@protonmail.com>
2023-10-27 11:52:53 +0200

    profil ajustement

--- profil.js
+++ profil.js
@@ -2,12 +2,13 @@ import { images } from "./images.js";
 
 window.onload = init;
 
-const choixMemory = "legumes";
+const nom = getUser().nom;
+let choixMemory = "legumes";
+
 function init(){
-    //todo : aller chercher dans le localstorage user
     afficherUser();
     document.getElementById("choixMemory").addEventListener("change", chargerImageMemory);
-
+    document.getElementById("enregistrerOption").addEventListener("click", ajouterPreferenceUtilisateur);
 
 }
 
@@ -15,21 +16,21 @@ function getUser() {
     return JSON.parse(window.localStorage.getItem("user") ?? "[]");
 }
 
+function getProfils() {
+    return JSON.parse(window.localStorage.getItem("profils") ?? "[]");
+}
+
+
 function afficherUser() {
-    const nom = getUser().nom;
     const email = getUser().email;
-    console.log(nom);
 
     document.getElementById("nomUtilisateurProfil").value = nom;
     document.getElementById("emailProfil").value = email;
 }
 
-function chargerImageMemory(event) {
-   // document.getElementById("visualisationMemoryChoisi").remove("img");
-    
-    const choixMemory = event.target.value;
+function chargerImageMemory(event) {    
+    choixMemory = event.target.value;
     const src = images.find(v => v.dossier === choixMemory).detail;
-    console.log(src);
     const img = document.createElement("img");
     img.setAttribute("src", src);
     document.getElementById("visualisationMemoryChoisi").appendChild(img);
@@ -41,13 +42,24 @@ function tailleMemory() {
     const longueurMemory = images.find(v => v.dossier === choixMemory).longueur;
     const nbLignes = Math.floor(longueurMemory * 2 / 4); //2 car on affiche 2 fois une image et 4 c'est le nombre d'image par ligne
 
-    for (let l = 2; l = nbLignes; l++) { // au minmum 2 lignes
+    for (let l = 2; l < nbLignes + 1; l++) { // au minimum 2 lignes
         const longueur = 4 * l;
         const option = document.createElement("option");
         option.setAttribute("value", longueur);
-        option.innerText(`4 * ${2}`);
+        option.setAttribute("id", l);
+        option.innerText = `4 * ${l}`;
         document.getElementById("tailleMemory").appendChild(option);
     }
 }
 
 // todo : envoyer sur le localStorage profils en ajoutant à cet utilisateur le choix du memory et la taille
+function ajouterPreferenceUtilisateur() {
+    const tailleMemory = document.getElementById("tailleMemory").value;
+    //todo : ajouter la taille du memory et le memory au Json getUser()
+
+    const profilConnecte = getProfils().find(e => e.nom === nom);
+    window.localStorage.removeItem(profilConnecte);
+
+    const updatedProfils = [...getProfils(), getUser()];
+    window.localStorage.setItem("profils", JSON.stringify(updatedProfils));
+}
